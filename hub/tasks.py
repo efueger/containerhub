29d3539c351bc4e6d7789bd1a6301fa949198456
Ricardo Band <email@ricardo.band>
2016-12-12 01:50:46 +0100

    network creation and deletion updated; could be ready but untestet

--- hub/tasks.py
+++ hub/tasks.py
@@ -17,22 +17,26 @@ def create_network(network: Network):
     :param network: the network that should be created
     :return:
     """
-    with open('/etc/netctl/hubnet_%s' % network.uuid.hex) as f:
-        config = 'Description="LXC bridge for hub network {uuid} of user {email}"\n' \
-                 'Interface={interface}\n' \
-                 'Connection=bridge\n' \
-                 'BindsToInterfaces=()\n' \
-                 'IP=static\n' \
-                 'Address={address}\n' \
-                 'SkipForwardingDelay=yes\n' \
-                .format(uuid=str(network.uuid),
-                        email=network.user.email,
-                        interface='hubnet_%s' % network.uuid.hex,
-                        address=network.network)
-        f.write(config)
+    config = 'Description="LXC bridge for hub network {uuid} of user {email}"\n' \
+             'Interface={interface}\n' \
+             'Connection=bridge\n' \
+             'BindsToInterfaces=()\n' \
+             'IP=static\n' \
+             'Address={address}\n' \
+             'SkipForwardingDelay=yes\n' \
+            .format(uuid=str(network.uuid),
+                    email=network.user.email,
+                    interface='hubnet_%s' % network.uuid.hex,
+                    address=network.network)
+
+    args = ['sudo', 'netctl_helper', 'add', network.uuid.hex]
+    p = subprocess.Popen(args, stdin=config)
+
     args = ['sudo', 'netctl', 'enable', 'hubnet_%s' % network.uuid.hex]
     p = subprocess.Popen(args)
-    os.system('netctl start hubnet_%s' % network.uuid.hex)
+
+    args = ['sudo', 'netctl', 'start', 'hubnet_%s' % network.uuid.hex]
+    p = subprocess.Popen(args)
 
 
 @shared_task()
@@ -44,9 +48,13 @@ def destroy_network(network: Network):
     :param network: the netowkr that should be destroyed
     :return:
     """
-    os.system('netctl stop hubnet_%s' % network.uuid.hex)
-    os.system('netctl disable hubnet_%s' % network.uuid.hex)
-    conf = '/etc/netctl/hubnet_%s' % network.uuid.hex
-    if os.path.isfile(conf):
-        os.unlink(conf)
+    args = ['sudo', 'netctl', 'stop', 'hubnet_%s' % network.uuid.hex]
+    p = subprocess.Popen(args)
+
+    args = ['sudo', 'netctl', 'disable', 'hubnet_%s' % network.uuid.hex]
+    p = subprocess.Popen(args)
+
+    args = ['sudo', 'netctl_helper', 'del', network.uuid.hex]
+    p = subprocess.Popen(args)
+
     network.delete()
